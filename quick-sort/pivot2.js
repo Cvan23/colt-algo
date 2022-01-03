fc0da54778554f89e59ce07ad55950ce22430748
Chris Van Huffe <cvanhuffel@outlook.com>
2022-01-02 20:08:36 -0800

    Quick Sort files added

---
+++ quick-sort/pivot2.js
@@ -0,0 +1,13 @@
+// Pivot Helper Example
+
+let arr = [ 5, 2, 1, 8, 4, 7, 6, 3 ]
+
+pivot(arr); // 4;
+
+arr;
+// any one of these is an acceptable mutation:
+// [2, 1, 4, 3, 5, 8, 7, 6]
+// [1, 4, 3, 2, 5, 7, 6, 8]
+// [3, 2, 1, 4, 5, 7, 6, 8]
+// [4, 1, 2, 3, 5, 6, 8, 7]
+// there are other acceptable mutations too!
